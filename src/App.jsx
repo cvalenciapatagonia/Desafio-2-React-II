171160f00fb9ba3f8cacbe025da1f0a47f566df8
Cristian Valencia G <117423462+cvalenciapatagonia@users.noreply.github.com>
2023-10-09 09:33:39 -0300

    Modificacion nombre ruta App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Data from "./components/Data";
-import "./app.css";
+import "./App.css";
 
 const App = () => {
   return (
